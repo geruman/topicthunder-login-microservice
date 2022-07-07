23bbccd43ba22517d6a780b191684b1734d00e31
German <bernhardt.german@gmail.com>
2022-07-07 20:19:15 -0300

    Application.kt ahora escucha en https://topic-thunder-backend.herokuapp.com/

--- src/main/kotlin/com/equipo7/Application.kt
+++ src/main/kotlin/com/equipo7/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.netty.*
 import com.equipo7.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 80, host = "0.0.0.0") {
+    embeddedServer(Netty, port = 80, host = "topic-thunder-backend.herokuapp.com") {
         configureRouting()
         configureSerialization()
     }.start(wait = true)
