6e5c6d816b91d27c1824d81416d6e85407cf4f2e
German <bernhardt.german@gmail.com>
2022-07-07 20:22:48 -0300

    Ahora application.kt y procfile toman el puerto de las variables de entorno

--- src/main/kotlin/com/equipo7/Application.kt
+++ src/main/kotlin/com/equipo7/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.netty.*
 import com.equipo7.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 80, host = "topic-thunder-backend.herokuapp.com") {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt(), host = "topic-thunder-backend.herokuapp.com") {
         configureRouting()
         configureSerialization()
     }.start(wait = true)
