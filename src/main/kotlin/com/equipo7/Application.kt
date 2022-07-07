b0827a2277c9813f4700315ed4fb75fc3826baa7
German <bernhardt.german@gmail.com>
2022-07-07 19:55:11 -0300

    Initial commit

---
+++ src/main/kotlin/com/equipo7/Application.kt
@@ -0,0 +1,12 @@
+package com.equipo7
+
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.equipo7.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
+        configureRouting()
+        configureSerialization()
+    }.start(wait = true)
+}
